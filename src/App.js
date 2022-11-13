7543a468ee56977900f0b04ffd87946481b1b3ce
Aung Kyaw Wai Htun <akwhtun@gmail.com>
2022-11-13 10:37:44 +0630

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Toolbar from './Toolbar';
+// class Item extends React.Component{
+//    render() {
+//       return <li>
+//          {this.props.name},
+//          ${this.props.price}
+//       </li>
+//    }
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class App extends React.Component{
+//    state = {
+//      items : [
+//          {id : 1, name : 'Apple', price: 2},
+//          {id : 2, name : 'Orange', price: 1.99},
+//          {id : 3, name : 'Banana', price: 1.19},
+//       ]
+//    };
+
+
+//    add = (name, price) => {
+//       let id = this.state.items.length + 1;
+//       this.setState({
+//          items: [
+//             ...this.state.items,
+//          { id,name,price}
+//         ]
+//       })
+//    }
+
+//    render() {
+//       return (<div>
+//          <h1>Hello React</h1>
+//          <ul>
+//             {this.state.items.map(i => {
+//                return (
+//                   <Item key={i.id} name={i.name} price={i.price} />
+//                )
+//             })}
+//          </ul>
+//          <Addform add={this.add} />
+//       </div>)
+//    }
+// }
+
+// class Addform extends React.Component{
+//    nameRef = React.createRef();
+//    priceRef = React.createRef();
+//    add = () => {
+//     let  name = this.nameRef.current.value;
+//    let  price = this.priceRef.current.value;
+
+//       this.props.add(name, price);
+
+//    }
+//    render() {
+//       return (<div>
+//          <input type="text" ref={this.nameRef} /> <br/>
+//          <input type="text" ref={this.priceRef} /> <br/>
+//          <button  onClick={this.add}>Add</button>
+//       </div>)
+//    }
+// }
+
+class App extends React.Component{
+   render() {
+      return (
+         <div>
+ <Toolbar>
+ <h1>Hello React</h1>
+ <h2>Component composition</h2>
+ </Toolbar>
+ </div>
+      )
+   }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+const store = createStore((state =[], action) => {
+  if (action.type==="ADD")
+  return [...state, action.item];
+  return state;
+});
+
+ReactDOM.render (
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
